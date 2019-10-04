207bc17ef7bca141d4de245f061f3e4bd8827433
NDari <naseerdari01@gmail.com>
2019-10-03 20:42:13 -0400

    add ohmyrepl

--- startup.jl
+++ startup.jl
@@ -3,24 +3,20 @@
 # will automatically run the initialization for us.
 isfile("_init.jl") && include(joinpath(pwd(), "_init.jl"))
 
-# set a local directory for personal code, and add it to the
-# load path so that we can easily use the modules.
-push!(LOAD_PATH,"$(homedir())/Projects/Julia" )
-
 # use revise. Automatically loads git, module, etc changes
 # when using the repl.
 atreplinit() do repl
+    try
+        @eval using Debugger
+    catch
+        @warn "Could not load Debugger."
+    end
+
     try
         @eval using Revise
         @async Revise.wait_steal_repl_backend()
     catch
         @warn "Could not load Revise."
-    end
-
-    try
-        @eval using Rebugger
-    catch
-        @warn "Could not load Rebugger."
     end
 end
 
