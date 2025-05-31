9c2531c9cb5614428cd9e397d054b8afb48a2f6b
Naseer Dari <naseerdari01@gmail.com>
2025-05-31 11:38:16 -0500

    after first reinstall of fedora

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -2,6 +2,13 @@
 
 set -eu pipefail
 
+# git
+sudo dnf install -y git
+git config --global user.email "naseerdari01@gmail.com"
+git config --global user.name "Naseer Dari"
+git config --global init.defaultbranch "main"
+git config --global core.whitespace "cr-at-eol"
+
 git clone https://github.com/NDari/new-dots.git $HOME/dotfiles
 
 # zsh
@@ -13,14 +20,6 @@ ln -s $HOME/dotfiles/zsh/zshrc $HOME/.config/zsh/.zshrc
 ln -s $HOME/dotfiles/zsh/zshenv $HOME/.zshenv
 # oh my posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
-
-
-# git
-sudo dnf install -y git
-git config --global user.email "naseerdari01@gmail.com"
-git config --global user.name "Naseer Dari"
-git config --global init.defaultbranch "main"
-git config --global core.whitespace "cr-at-eol"
 
 # nvim
 cd $HOME
@@ -35,12 +34,23 @@ cd $HOME
 mkdir -p $HOME/.config/nvim
 ln -s $HOME/dotfiles/nvim/init.lua $HOME/.config/nvim/init.lua
 
-sudo dnf install -y fzf direnv
-
 mkdir $HOME/.ssh
+
+sudo dnf install -y \
+	fzf \
+	direnv \
+	tmux \
+	xclip \
+	ripgrep \
+	fd-find \
+	uv
+
+# tmux
+cd $HOME
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 
 # go
 mkdir -p $HOME/tools && cd $HOME/tools
 curl -LO https://go.dev/dl/go1.24.3.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.24.3.linux-amd64.tar.gz
-
